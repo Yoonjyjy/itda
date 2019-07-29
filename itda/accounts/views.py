7aeda0b1ef4b345957a83b305c37d703a7612b83
yoonjyjy <diniless@naver.com>
2019-07-29 20:59:44 +0900

    first commit

---
+++ itda/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import views
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import auth
+
+# Create your views here.
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\윤지영\desktop\itda\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
